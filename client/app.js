ea0872b41f0717f083f88769d274caa82775a8a0
Matt Pitts <mattpittsmusic@gmail.com>
2017-06-02 14:21:04 -0600

    basic front page working

--- client/app.js
+++ client/app.js
@@ -4,10 +4,20 @@ $(document).ready(function() {
 
 
 function getMaturationPackages() {
-	$.get('hhtp://localhost:3000/info').then(createCards);
+	$.get('http://localhost:3000/info').then(createCards);
 }
 
 
 function createCards(data) {
-	console.log(data);
+	// console.log(data);
+	data.forEach(function(item) {
+		let name = item.name;
+		let price = item.price;
+		let description = item.description;
+		let $newMaturationCard = $(`<div class="col s12 m3"></div>`)
+			.append($(`<h5>${name}</h5>`))
+			.append($(`<p>${price}</p>`))
+			.append($(`<p class="light">${description}</p>`));
+		$('#packages-container').append($newMaturationCard);
+	});
 }
